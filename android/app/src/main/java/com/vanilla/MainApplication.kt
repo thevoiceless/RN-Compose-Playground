a163f66bbeb2b30b8027240f3e4e0015c59975bb
Riley Moses <riley.moses@discordapp.com>
2023-05-24 17:52:12 -0600

    Update to latest RN and Compose

--- android/app/src/main/java/com/vanilla/MainApplication.kt
+++ android/app/src/main/java/com/vanilla/MainApplication.kt
@@ -1,79 +1,43 @@
 package com.vanilla
 
 import android.app.Application
-import android.content.Context
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.PackageList
-import com.vanilla.newarchitecture.MainApplicationReactNativeHost
-import com.facebook.react.config.ReactFeatureFlags
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import com.facebook.react.ReactInstanceManager
-import com.vanilla.ReactNativeFlipper.initializeFlipper
-import java.lang.reflect.InvocationTargetException
 
 class MainApplication : Application(), ReactApplication {
-    private val legacyArchitectureHost = object : ReactNativeHost(this) {
+    private val mReactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
         override fun getUseDeveloperSupport() = BuildConfig.DEBUG
 
         override fun getPackages(): List<ReactPackage> {
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
+            // Packages that cannot be autolinked yet can be added manually here
             return PackageList(this).packages.apply {
                 add(VanillaAppPackage())
             }
         }
 
         override fun getJSMainModuleName() = "index"
+
+        override val isNewArchEnabled = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+
+        override val isHermesEnabled = BuildConfig.IS_HERMES_ENABLED
     }
-    private val newArchitectureHost = MainApplicationReactNativeHost(this)
+
     override fun getReactNativeHost(): ReactNativeHost {
-        return if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            newArchitectureHost
-        } else {
-            legacyArchitectureHost
-        }
+        return mReactNativeHost
     }
 
     override fun onCreate() {
         super.onCreate()
-        // If you opted-in for the New Architecture, we enable the TurboModule system
-        ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        SoLoader.init(this, false)
-        initializeFlipper(this, reactNativeHost.reactInstanceManager)
-    }
-
-    companion object {
-        /**
-         * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-         * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-         */
-        private fun initializeFlipper(context: Context, reactInstanceManager: ReactInstanceManager) {
-            if (BuildConfig.DEBUG) {
-                try {
-                    /*
-                    We use reflection here to pick up the class that initializes Flipper,
-                    since Flipper library is not available in release mode
-                    */
-                    val aClass = Class.forName("com.vanilla.ReactNativeFlipper")
-                    aClass
-                        .getMethod(
-                            "initializeFlipper",
-                            Context::class.java,
-                            ReactInstanceManager::class.java
-                        )
-                        .invoke(null, context, reactInstanceManager)
-                } catch (e: ClassNotFoundException) {
-                    e.printStackTrace()
-                } catch (e: NoSuchMethodException) {
-                    e.printStackTrace()
-                } catch (e: IllegalAccessException) {
-                    e.printStackTrace()
-                } catch (e: InvocationTargetException) {
-                    e.printStackTrace()
-                }
-            }
+        SoLoader.init(this,  /* native exopackage */false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            load()
         }
+        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
     }
 }
